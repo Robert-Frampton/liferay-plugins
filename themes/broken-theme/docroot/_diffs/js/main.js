94a51d9b41484185fe902dd598ef21c95647b006
Matthew Longinow <Matthew.longinow@liferay.com>
2014-03-05 14:53:31 -0800

    Mat's broken-theme-test

--- themes/broken-theme/docroot/_diffs/js/main.js
+++ themes/broken-theme/docroot/_diffs/js/main.js
@@ -1,5 +1,5 @@
 AUI().ready(
-	'liferay-hudcrumbs', 'liferay-navigation-interaction', 'liferay-sign-in-modal',
+	'aui-modal', 'aui-dialog-iframe-deprecated', 'aui-io-plugin-deprecated', 'liferay-util-window', 'aui-base', 'liferay-hudcrumbs', 'liferay-navigation-interaction', 'liferay-sign-in-modal',
 	function(A) {
 		var navigation = A.one('#navigation');
 
@@ -13,12 +13,12 @@ AUI().ready(
 			siteBreadcrumbs.plug(A.Hudcrumbs);
 		}
 
-		A.getBody().delegate('click', eventHandler, 'a.logo');
-
 		var eventHandler = function(event) {
 			event.preventDefault();
 			alert(event.currentTarget.attr('title'));
 		}
+
+		A.getBody().delegate('click', eventHandler, 'a.logo');
 
 		var signIn = A.one('li.sign-in a');
 
@@ -28,9 +28,85 @@ AUI().ready(
 
 		var poweredByLink = A.one('#poweredByLink');
 
-		poweredByLink.on('click', function(event) {
-			event.preventDefault();
-			window.open('https://www.liferay.com/', 'Liferay');
-		});
+		// Liferay.provide (
+		// 	window,
+		// 	'popup',
+		// 	function() {
+		// 		var A = AUI();
+		// 		var portletURL = 'https://www.liferay.com/';
+		// 		var dialog = new A.Dialog (
+		// 			{
+		// 				destroyOnClose: true,
+		// 				modal: false,
+		// 				title: 'Liferay',
+		// 				width: 900
+		// 			}
+		// 		).plug(
+		// 			A.Plugin.IO,
+		// 				{
+		// 					uri: portletURL.toString()
+		// 				}
+		// 		).render();
+		// 	}
+		// 	['aui-base', 'aui-io-plugin-deprecated', 'aui-dialog-iframe-deprecated', 'liferay-util-window']
+		// );
+
+		// poweredByLink.on (
+		// 	'click',
+		// 	function(event) {
+		// 		window['popup'];
+		// 	}
+		// );
+
+		poweredByLink.on(
+			'click',
+			function(event) {
+				event.preventDefault();
+
+				var winWidth = 750;
+				var winHeight = 600;
+				var positionLeft = (screen.availWidth/2) - (winWidth/2);
+				var positionTop = (screen.availHeight/2) - (winHeight/2);
+				var settings = 'height=' + winHeight + ',width=' + winWidth + ',top=' + positionTop + ',left=' + positionLeft + '';
+				window.open('https://www.liferay.com/', '_blank', settings);
+
+			}
+		);
+
+		// function popup(url) {
+		// 	Liferay.Util.Window.openWindow (
+		// 		{
+		// 			centered: true,
+		// 			constrain2view: true,
+		// 			modal: true,
+		// 			resizable: true,
+		// 			width: 750,
+		// 			title: 'http://www.liferay.com'
+		// 		}
+		// 	).plug(
+		// 		A.Plugin.IO,
+		// 		{
+		// 			uri: url
+		// 		}
+		// 	).render();
+		// }
+
+		// poweredByLink.on(
+		// 	'click',
+		// 	function(event) {
+		// 		event.preventDefault();
+
+		// 		popup('http://www.liferay.com');
+
+		// });
+
+		// var modalPopup = new A.Modal (
+		// 	{
+		// 		bodyContent: 'Modal body,
+		// 		centered: true,
+		// 		headerContent: 'Modal header',
+		// 		render: 'https://www.liferay.com/'
+		// 	}
+		// ).render();
 	}
 );
