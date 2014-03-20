fcdbdfe73fea7998e24ad0e99d62186c86664737
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2014-03-19 17:28:51 -0700

    For Radiate Theme

---
+++ themes/Radiate/docroot/js/main.js
@@ -0,0 +1,31 @@
+AUI().ready(
+	'liferay-hudcrumbs', 'liferay-navigation-interaction', 'liferay-sign-in-modal', 'aui-toggler',
+	function(A) {
+		var navigation = A.one('#navigation');
+
+		if (navigation) {
+			navigation.plug(Liferay.NavigationInteraction);
+		}
+
+		var siteBreadcrumbs = A.one('#breadcrumbs');
+
+		if (siteBreadcrumbs) {
+			siteBreadcrumbs.plug(A.Hudcrumbs);
+		}
+
+		var signIn = A.one('li.sign-in a');
+
+		if (signIn && signIn.getData('redirect') !== 'true') {
+			signIn.plug(Liferay.SignInModal);
+		}
+
+		new A.Toggler(
+    	  {
+        	container: '#myToggler',
+        	content: '.content1',
+        	expanded: false,
+        	header: '.header1'
+          }
+    	);
+	}
+);
