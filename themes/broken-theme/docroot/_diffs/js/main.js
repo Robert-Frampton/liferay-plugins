b7e0cef3521a72aff1cddfaa2b2341f40a6296f5
Rebecca Dai <rebecca.dai@liferay.com>
2014-05-08 15:08:24 -0700

    broken-theme feature request in process

--- themes/broken-theme/docroot/_diffs/js/main.js
+++ themes/broken-theme/docroot/_diffs/js/main.js
@@ -1,5 +1,5 @@
 AUI().ready(
-	'liferay-hudcrumbs', 'liferay-navigation-interaction', 'aui-io-request', 'node', 'aui-modal',
+	'liferay-hudcrumbs', 'liferay-navigation-interaction', 'liferay-sign-in-modal', 'aui-io-request', 'node','aui-modal',
 	function(A) {
 		var navigation = A.one('#navigation');
 
@@ -13,33 +13,46 @@ AUI().ready(
 			siteBreadcrumbs.plug(A.Hudcrumbs);
 		}
 
-		A.getBody().delegate('click', eventHanler, 'a.logo');
+		var signIn = A.one('li.sign-in a');
 
-		var eventHandler function(event) {
+		if (signIn && signIn.getData('redirect') !== 'true') {
+			signIn.plug(Liferay.SignInModal);
+		}
+
+		var eventHandler = function(event) {
 			event.preventDefault();
 			alert(event.currentTarget.attr('title'));
 		}
 
-		A.io.request(
-			'',
-			{
-				dataType: 'json',
-				on: {
-					success: function() {
-						var data = this.get('responseData');
-						alert(data);
+		A.getBody().delegate('click', eventHandler, 'a.logo');
+
+
+		var crumbSignIn = function(event) {
+			event.preventDefault();
+			
+			var crumbSignIn = A.io.request(
+				'http://localhost:8080/web/guest/home?p_p_id=58&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view&saveLastPath=false&_58_struts_action=%2Flogin%2Flogin',
+				{
+					on: {
+						success: function() {
+							var responseData = this.get('responseData');
+
+							var signInModal = new A.Modal(
+							{
+								bodyContent: responseData,
+								centered: true,
+								height: 790,
+								modal: true,
+								render: '#signInModal',
+								width: 590
+							}
+							).render();
+						}
 					}
 				}
-			}
-		);
+			);
+		}
+	A.getBody().delegate ('click', eventHandler, siteBreadcrumbs);
 
-		var signinModal = new A.Modal(
-			{
-				bodyContent: '<iframe src="http://localhost:8080/c/portal/login?p_l_id=10190"></iframe>',
-				centered: true,
-				modal: true,
-				render: '#signinModal',
-			}
-			).render();
 	}
 );
